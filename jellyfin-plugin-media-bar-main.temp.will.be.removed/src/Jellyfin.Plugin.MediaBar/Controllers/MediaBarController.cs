8c2d4724418c8db757e8994acf64a6cb5f11ff1c
gͥhoͣsͫt <55955972+GG-GhostGaming@users.noreply.github.com>
2026-09-15 13:42:20 -0400

    Add files via upload

---
+++ jellyfin-plugin-media-bar-main.temp.will.be.removed/src/Jellyfin.Plugin.MediaBar/Controllers/MediaBarController.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+using Jellyfin.Extensions;
+using Jellyfin.Plugin.MediaBar.Configuration;
+using Jellyfin.Plugin.MediaBar.Helpers;
+using Jellyfin.Plugin.MediaBar.Model;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Playlists;
+using Microsoft.AspNetCore.Mvc;
+using MediaBrowser.Controller.Entities.TV;
+
+namespace Jellyfin.Plugin.MediaBar.Controllers
+{
+    [Route("[controller]")]
+    public class MediaBarController : ControllerBase
+    {
+        [HttpGet("{file}")]
+        public ActionResult GetFile([FromRoute] string file)
+        {
+            Stream? fileStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Jellyfin.Plugin.MediaBar.Inject." + file);
+
+            if (fileStream == null)
+            {
+                return NotFound();
+            }
+
+            using StreamReader streamReader = new StreamReader(fileStream);
+            string fileContents = streamReader.ReadToEnd();
+
+            string contentType = "text/plain";
+
+            if (Path.GetExtension(file) == ".js")
+            {
+                contentType = "text/javascript";
+            }
+            else if (Path.GetExtension(file) == ".css")
+            {
+                contentType = "text/css";
+            }
+            else if (Path.GetExtension(file) == ".html")
+            {
+                contentType = "text/html";
+            }
+        
+            return Content(fileContents, contentType);
+        }
+
+        [HttpPost("Avatar/List")]
+        public ActionResult GetAvatarsList([FromBody] PatchRequestPayload payload)
+        {
+            string? content = TransformationPatches.AvatarsList(payload);
+
+            if (content == null)
+            {
+                return NotFound();
+            }
+            
+            return Content(content, "text/plain");
+        }
+
+        [HttpGet("WebConfig")]
+        public ActionResult<WebConfig> GetWebConfig()
+        {
+            return Ok(MediaBarPlugin.Instance.Configuration.WebConfig);
+        }
+    }
+}
